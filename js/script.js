02332e291aedee6bd3ff06c10909acaf396993b7
LowUp <60651716+LowUp@users.noreply.github.com>
2021-04-12 19:28:51 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-<!--- Javascript for toggle menu ----->
-jQuery(document).ready(function(){
-  jQuery(".menu_icon").click(function(){
-    if(jQuery("nav ul").css({"max-height": "200px"}))
-	{jQuery("nav ul").fadeToggle();}
-  });
-});
